bc4cf407fe9f919da1d2eb50e8d7aec1d458fd1f
iorymaeda <86793373+iorymaeda@users.noreply.github.com>
2022-09-24 15:49:44 +0300

    implement train code and stuff

--- train.py
+++ train.py
@@ -1,43 +1,144 @@
-import yaml
-import time
+import json
+import pprint
+import datetime
+from contextlib import suppress
 
+import tqdm
 import torch
+import torchsummary
+import torchmetrics
+from torch.utils.tensorboard import SummaryWriter
+from torch.utils.data import DataLoader
 
 import utils
-
-
-if __name__ == "__main__":
-    try:
-        with open('config.yml', 'r') as stream:
-            config = yaml.safe_load(stream)
-    except FileNotFoundError:
-        with open('config.yaml', 'r') as stream:
-            config = yaml.safe_load(stream)
-
-
-    wave_transforms = [
-        utils.augmentation.wave.RIR(config['sample_rate'], p=0.5),
-        utils.augmentation.wave.LPHPFilter(config['sample_rate'], p=0.5),
-        utils.augmentation.wave.TimeShift(None, p=0.5),
-        utils.augmentation.wave.AdditiveUN(min_snr_db=30, max_snr_db=1, p=0.8)
-    ]
-
-    spec_transforms = [
-        utils.augmentation.spec.DropBlock2D(0.5, 16),
-        utils.augmentation.spec.TimeMasking(90, 5, 0.5),
-        utils.augmentation.spec.FrequencyMasking(5, 5, 0.5),
-    ]
-
-    esc_dataset = utils.datasets.ESCDataset(
-        audio_length=5,
-        folds=[1, 2, 3],
-        wave_transforms=wave_transforms,
-        spec_transforms=spec_transforms,
-    )
     
-    start_time = time.time()
-    for _ in range(128):
-        x, y = esc_dataset[_]
+def get_writer(name:str):
+    log_dir = f"logs/fit/esc50/{name}"
+    writer = SummaryWriter(log_dir)
+    return writer
+
+@torch.no_grad()
+def compute_metrics(y_pred: torch.Tensor, y_true: torch.Tensor):
+    y_pred, y_true = y_pred.cpu(), y_true.cpu()
+    num_classes = y_pred.shape[1]
+    return {
+        'Acc': float(torchmetrics.functional.accuracy(y_pred, y_true, top_k=1, num_classes=num_classes)),
+        'Acc5': float(torchmetrics.functional.accuracy(y_pred, y_true, top_k=5, num_classes=num_classes)),
+        'AUC': float(torchmetrics.functional.auroc(y_pred, y_true, num_classes=num_classes)),
+    }
+
+
+def train():
+    config = utils.tools.ConfigBase._get_config()
+    date = datetime.datetime.now().strftime("%Y.%m.%d - %H-%M")
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    print(date)
+    print(device)
+    
+    results = {}
+    for fold in range(5):
+        torch.cuda.empty_cache()
         
-    print(time.time() - start_time)
+        folds = [x for x in range(1, 6)]
+        rm_fold = folds.pop(fold)
+
+        writer = None
+        name = f'Fold {rm_fold} {date}'
+        print(f"Start train fold:", rm_fold)
+        # --------------------------------------------------------- #
+        train_dataset = utils.datasets.ESCDataset(
+            audio_length=5,
+            folds=folds,
+        )
+        test_dataset = utils.datasets.ESCDataset(
+            audio_length=5,
+            folds=[rm_fold],
+        )
+        
+        num_classes = train_dataset.meta['y'].max()+1
+        train_loader = DataLoader(
+            train_dataset, 
+            batch_size=config['train']['batch_size'],
+            num_workers=4,
+            drop_last=True,
+            shuffle=True,
+            collate_fn=train_dataset.collate_fn
+        )
+        test_loader = DataLoader(
+            test_dataset, 
+            batch_size=config['train']['batch_size'],
+            num_workers=4,
+            drop_last=False,
+            shuffle=False,
+            collate_fn=test_dataset.collate_fn
+        )
+        
+        # --------------------------------------------------------- #
+        for batch in DataLoader(train_dataset, batch_size=1, num_workers=0, collate_fn=train_dataset.collate_fn):
+            x, y = batch
+            break
+
+        model = utils.nn.Model(x.shape, num_classes=num_classes, build_verbose=True if fold == 0 else False).to('cuda')
+        optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4, weight_decay=0.1)
+        
+        if fold == 0:
+            torchsummary.summary(model)
+
+        trainer = utils.trainers.SupervisedClassification(
+            model=model,
+            device=device,
+            optimizer=optimizer,
+            loss_fn=torch.nn.CrossEntropyLoss(),
+            accum_iter=config['train']['accum_iter'],
+            grad_clip=config['train']['grad_clip'],
+            metrics={
+                "Acc": torchmetrics.Accuracy(num_classes=num_classes),
+                "AUC": torchmetrics.AUROC(num_classes=num_classes),
+            }
+        )
+        
+        # --------------------------------------------------------- #
+        # Train
+        wait = 0
+        epoch = 0
+        best_score = -torch.inf
+        best_metrics = None
+        with suppress(KeyboardInterrupt):
+            print("start train")
+            while wait < config['train']['patience']:
+                # --------------------------------------------- #
+                # Train
+                train_loss = trainer.train_epoch(train_loader)
+                if writer is None: writer = get_writer(name)
+
+                writer.add_scalar('Acc/train', trainer.metrics['Acc'].compute(), epoch)
+                writer.add_scalar('AUC/train', trainer.metrics['AUC'].compute(), epoch)
+
+                # --------------------------------------------- #
+                # Test    
+                test_pred, test_true = trainer.evaluate(test_loader)
+                test_pred = test_pred.softmax(dim=1)
+                test_metrics = compute_metrics(test_pred, test_true)
+                for _metric in test_metrics:
+                    writer.add_scalar(f'{_metric}/test', test_metrics[_metric], epoch)
+                    
+                # --------------------------------------------- #
+                # EarlyStopping
+                wait, epoch = wait+1, epoch+1
+                if test_metrics['AUC'] > best_score:
+                    torch.save(trainer.checkpoint(), f'weights/esc50/{name}.torch')
+                    best_score = test_metrics['AUC']
+                    best_metrics = test_metrics
+                    wait = 0
+            
+        results[rm_fold] = [name, best_metrics]
     
+    with open(f'logs/results/{date}.json', 'w') as fp:
+        json.dump(results, fp)
+
+    pprint.pprint(results)
+
+
+if __name__ == '__main__':
+    train()
--- utils/nn.py
+++ utils/nn.py
@@ -1,8 +1,26 @@
-import torch
-import torch.nn.functional as F
-from torch import nn
+from typing import Literal
 
-from tools import ConfigBase
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+from .tools import ConfigBase
+from .augmentation import spec
+from torch.utils.checkpoint import checkpoint
+
+
+def get_activation(act: str):
+    match act.lower():
+        case 'gelu':
+            return nn.GELU()
+        case 'relu':
+            return nn.ReLU(),
+        case 'lrelu':
+            return nn.LeakyReLU(0.01)
+        case 'swiglu':
+            return SwiGLU()
+        case _:
+            raise Exception("Uncorrect activation function name")
 
 
 class SwiGLU(nn.Module):
@@ -16,76 +34,221 @@ class SwiGLU(nn.Module):
 
 
 class DropBlock2D(nn.Module):
-    r"""Randomly zeroes 2D spatial blocks of the input tensor.
-    As described in the paper
-    `DropBlock: A regularization method for convolutional networks`_ ,
-    dropping whole blocks of feature map allows to remove semantic
-    information as compared to regular dropout.
-    Args:
-        drop_prob (float): probability of an element to be dropped.
-        block_size (int): size of the block to drop
-    Shape:
-        - Input: `(N, C, H, W)`
-        - Output: `(N, C, H, W)`
-    .. _DropBlock: A regularization method for convolutional networks:
-       https://arxiv.org/abs/1810.12890
-
-    source: https://github.com/miguelvr/dropblock/blob/master/dropblock/dropblock.py
-    """
-
     def __init__(self, drop_prob, block_size):
         super(DropBlock2D, self).__init__()
-
-        self.drop_prob = drop_prob
-        self.block_size = block_size
-        self.gamma = self.drop_prob / (self.block_size ** 2)
+        self.drop_block = spec.DropBlock2D(drop_prob, block_size)
 
     def forward(self, x: torch.Tensor):
         # shape: (bsize, channels, height, width)
+        return self.drop_block(x)
 
-        assert x.dim() == 4, \
-            "Expected input with 4 dimensions (bsize, channels, height, width)"
 
-        if not self.training or self.drop_prob == 0.:
-            return x
+class GlobalPooling(nn.Module):
+    """Global pooling 
+    
+    Args:
+        mode (str): pooling mode any of: `avg`, `max`, `sum`, `mul`, `swiglu`, `swiglu2`
+            avg - average pooling
+            max - max pooling
+            sum - sum of avg and max pooling
+            mul - mul of avg and max pooling
+    Shape:
+        - Input: `(N, C, T, F)`
+        - Output: `(N, E)`
+
+    source: [2] II. AUDIO TAGGING SYSTEMS A. CNNs
+    """
+    def __init__(self, mode: Literal['avg', 'max', 'sum', 'mul'] = None, flat=False):
+        super(GlobalPooling, self).__init__()
+        self.mode = mode
+        self.flat = flat
+
+    def forward(self, x: torch.Tensor, mode=None, flat=None):
+        # |x| : (B, C, T, F)
+        if mode is None: mode = self.mode
+        if flat is None: flat = self.flat
+        assert mode is not None
+        assert flat is not None
+
+        if flat:
+            x = self.flatten(x)
+
+        match self.mode:
+            case 'avg':
+                return self.avg(x)
+            case 'max':
+                return self.max(x)
+            case 'sum':
+                return self.avg(x) + self.max(x)
+            case 'mul':
+                return self.avg(x) * self.max(x)
+    
+    def flatten(self, x: torch.Tensor):
+        """Flatten x:
+        B, C, T, F = x.shape
+        x.view(B, T, C*F)
+        """
+        return x.transpose(2, 3).flatten(1, 2).transpose(1, 2)
+
+    def avg(self, x: torch.Tensor):
+        shape = x.shape
+        if len(shape) == 4:
+            return x.mean(dim=[1, 2])
+        elif len(shape) == 3:
+            return x.mean(dim=1)
         else:
-            # sample mask
-            mask = (torch.rand(x.shape[0], *x.shape[2:]) < self.gamma).float()
+            raise Exception(f"Unexecpted shape: {shape}")
 
-            # place mask on input device
-            mask = mask.to(x.device)
+    def max(self, x: torch.Tensor):
+        shape = x.shape
+        if len(shape) == 4:
+            return x.max(dim=1).values.max(dim=1).values
+        elif len(shape) == 3:
+            return x.max(dim=1).values
+        else:
+            raise Exception(f"Unexecpted shape: {shape}")
 
-            # compute block mask
-            block_mask = self._compute_block_mask(mask)
 
-            # apply block mask
-            out = x * block_mask[:, None, :, :]
+class ResBlock(nn.Module):
+    def __init__(self, 
+        kernel_size: list[int], kernel_size_res: list[int], activation: str, 
+        c_in: int, c_emb: int, c_out: int, batch_first:bool=True):
+        super(ResBlock, self).__init__()
 
-            # scale output
-            out = out * block_mask.numel() / block_mask.sum()
+        dilation = [2 - kernel_size[0]%2, 2 - kernel_size[0]%2]
+        dilation_res = [2 - kernel_size_res[0]%2, 2 - kernel_size_res[0]%2]
+        
+        self.batchnorm_1 = nn.BatchNorm2d(c_in)
+        self.batchnorm_2 = nn.BatchNorm2d(c_emb)
+        
+        self.conv1 = nn.Conv2d(c_in, c_emb, kernel_size, dilation=dilation, padding='same')
+        self.conv2 = nn.Conv2d(c_emb, c_out, kernel_size, dilation=dilation, padding='same')
+        self.conv_res = nn.Conv2d(c_in, c_out, kernel_size_res, dilation=dilation_res, padding='same')
 
-            return out
-
-    def _compute_block_mask(self, mask: torch.Tensor):
-        block_mask = F.max_pool2d(input=mask[:, None, :, :],
-                                  kernel_size=(self.block_size, self.block_size),
-                                  stride=(1, 1),
-                                  padding=self.block_size // 2)
-
-        if self.block_size % 2 == 0:
-            block_mask = block_mask[:, :, :-1, :-1]
-
-        block_mask = 1 - block_mask.squeeze(1)
-
-        return block_mask
+        self.batch_first = batch_first
+        self.act = get_activation(activation)
+        
+    def forward(self, x: torch.Tensor):
+        # |x| : (batch_size, C, H, W)
+        res = self.conv_res(x)
+        
+        # ------------------------- #
+        if self.batch_first:
+            x = self.batchnorm_1(x)
+            x = self.act(x)
+        else:
+            x = self.act(x)
+            x = self.batchnorm_1(x)
+        x = self.conv1(x)
+    
+        # ------------------------- #
+        if self.batch_first:
+            x = self.batchnorm_2(x)
+            x = self.act(x)
+        else:
+            x = self.act(x)
+            x = self.batchnorm_2(x)
+        x = self.conv2(x)
+        
+        return x + res
 
 
 class Model(ConfigBase, nn.Module):
-    def __init__(self, input_shape: tuple[int, int]):
+    chckpoint_blocks = (ResBlock, nn.Linear)
+
+    @torch.no_grad()
+    def __init__(self, input_shape: list[int, int, int, int], num_classes:int, build_verbose=False):
         super(Model, self).__init__()
+        # |input_shape| : (B, C, T, F)
 
-        self.input_shape = input_shape
+        config = self._get_config()
+        config = config['model']
 
+        self.verbose = False
+        self.use_checkpoint = config['use_checkpoint']
 
+        self.blocks = nn.ModuleList()
+        tensor: torch.Tensor = torch.rand(*tuple(input_shape))
+        get_key = lambda x: list(x.keys())[0]
+
+        if build_verbose:
+            print("Input shape")
+            print(f"{tensor.shape=}")
+            print()
+
+        for block in config['blocks']:
+            assert isinstance(block, dict)
+
+            key: str = get_key(block)
+            values = block[key]
+            match key.lower():
+                case 'res':
+                    _block = ResBlock(
+                        **config['res_block'],
+                        c_in=tensor.shape[1], 
+                        c_emb=values[0], 
+                        c_out=values[1]
+                    )
+                    tensor = _block(tensor)
+                    self.blocks.append(_block)
+                    
+                case 'max_pool':
+                    _block = nn.MaxPool2d(kernel_size=[values[0], 1], stride=[values[1], 1])
+                    tensor = _block(tensor)
+                    self.blocks.append(_block)
+
+                case 'drop':
+                    _block = DropBlock2D(values[0], values[1:])
+                    tensor = _block(tensor)
+                    self.blocks.append(_block)
+
+                case 'pooling':
+                    _block = GlobalPooling(**values)
+                    tensor = _block(tensor)
+                    self.blocks.append(_block)
+                
+                case 'fc':
+                    _block = nn.Linear(tensor.shape[-1], values)
+                    tensor = _block(tensor)
+                    self.blocks.append(_block)
+
+                case 'activation':
+                    _block = get_activation(values)
+                    tensor = _block(tensor)
+                    self.blocks.append(_block)
+
+                case _:
+                    raise Exception(f"Unexcepted block: {key}")
+
+            if build_verbose:
+                print(f"block={type(_block).__name__}")
+                print(f"{tensor.shape=}")
+                print()
+
+        self.blocks.append(nn.Linear(tensor.shape[-1], num_classes))
+
+    def forward(self, x: torch.Tensor):
+        # |x| : (B, C, T, F)
+        if self.verbose:
+            print("Input shape")
+            print(f"{x.shape=}")
+            print()
+        
+        if self.use_checkpoint:
+            x = torch.autograd.Variable(x, requires_grad=True)
+
+        for block in self.blocks:
+            if self.use_checkpoint and isinstance(block, self.chckpoint_blocks):
+                x = checkpoint(block, x)
+                
+            else:
+                x = block(x)
+
+            if self.verbose:
+                print(f"block={type(block).__name__}")
+                print(f"{x.shape=}")
+                print()
+
+        return x
 
     
